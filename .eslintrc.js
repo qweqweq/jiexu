94d6d9bb242b59ffcd92fdca2b2765a581b778fa
allen <1234@qq.com>
2019-11-10 11:51:39 +0800

    add develop branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,9 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'semi': [2, 'never'],
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'prettier/prettier': ['error', { 'semi': false }]
   }
 }
