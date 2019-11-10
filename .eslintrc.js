720ea6db580570ddbbd050eae31356a443326420
allen <1234@qq.com>
2019-11-10 11:55:52 +0800

    fix config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true
+  },
+  'extends': 'vuetify',
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
-  rules: {
-    'semi': [2, 'never'],
-    'no-console': 'off',
-    'vue/max-attributes-per-line': 'off',
-    'prettier/prettier': ['error', { 'semi': false }]
   }
 }
