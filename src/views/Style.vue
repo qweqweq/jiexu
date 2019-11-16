53bfdef411c1d8d86af366dc54a4d98f1869be67
allen <1234@qq.com>
2019-11-17 00:21:05 +0800

    fix style

--- src/views/Style.vue
+++ src/views/Style.vue
@@ -17,6 +17,7 @@
           align-center
           justify-center
           column
+          style="text-align:center"
         >
           <p class="title light-blue--text lighten-1">
             十余年管理类联考资深授课教师
@@ -29,11 +30,48 @@
           row
           wrap
           style="margin:0 8%;"
+          class="hidden-sm-and-down"
         >
           <v-flex
             v-for="(item, i) in teachers"
             :key="i"
             xs3
+          >
+            <v-card
+              class="pa-3 ma-2"
+              style="height: 680px"
+            >
+              <v-img
+                :src="require('@/assets/imgs/teacher/'+ item.avator)"
+                aspect-ratio="1"
+              />
+              <v-card-title primary-title>
+                <v-layout
+                  column
+                  align-center
+                  justify-center
+                  class="light-blue--text darken-4"
+                >
+                  <div class="headline">
+                    {{ item.name }}
+                  </div>
+                  <div>{{ item.title }}</div>
+                  <div>{{ item.text }}</div>
+                </v-layout>
+              </v-card-title>
+              <div>{{ item.desc }}</div>
+            </v-card>
+          </v-flex>
+        </v-layout>
+        <v-layout
+          row
+          wrap
+          style="margin:0;"
+          class="hidden-lg-and-up"
+        >
+          <v-flex
+            v-for="(item, i) in teachers"
+            :key="i"
           >
             <v-card
               class="pa-3 ma-2"
