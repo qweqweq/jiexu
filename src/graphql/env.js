bb4fcaa53040c56f8fb43907915dca84c07a5e3b
allen <1234@qq.com>
2020-06-20 21:20:37 +0800

    fix env

--- src/graphql/env.js
+++ src/graphql/env.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV;
 const CUSTOM_HOST = {
     'develop': 'http://localhost:1337',
-    'beta': 'http://139.219.137.131:8080',
-    'production': 'http://www.jiexu.com'
+    'beta': 'http://admin.jiexuedu.com:8080',
+    'production': 'http://admin.jiexuedu.com'
 };
 export default CUSTOM_HOST[env];
